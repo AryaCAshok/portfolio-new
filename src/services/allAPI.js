9a2e57dcd6db79df08466e11bc1a72a9622eeb47
Aryakakkove <aryac7510@gmail.com>
2024-03-02 22:54:44 +0530

    service deleted

--- src/services/allAPI.js
+++
@@ -1,8 +0,0 @@
-import { serverURL } from "./serverURLnew";
-import { commonAPI } from "./commonAPI";
-
-// 1) upload video
-
-export const uploadMessage =async(reqBody)=>{
-   return await commonAPI('POST',`${serverURL}/message`,reqBody)
-}
